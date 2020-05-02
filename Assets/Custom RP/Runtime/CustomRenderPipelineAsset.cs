ddc1bf654617c5404a8881e210a2b7ffab8ac790
South-Walker <506830915@qq.com>
2020-05-02 15:53:59 +0800

    create first srp asset

---
+++ Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+//可以通过menu中的Assets/Rendering/Custom Render Pipeline创建对应Asset
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return null;
+    }
+}
